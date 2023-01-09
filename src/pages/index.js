43b1f0223acd4640635229dd6444ec9f91dca0c4
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2023-01-09 10:27:23 -0800

    deleted s

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div>
       <Head>
-        <title>Amazon 2.0s</title>
+        <title>Amazon 2.0</title>
       </Head>
 
       {/* ---- TO BEGIN, delete this section and GET CODING!!! ---- */}
