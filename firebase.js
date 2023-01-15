cd35bc75a804ac1f1256959ddae3c6b24a1398ea
ericlam1114 <83894250+ericlam1114@users.noreply.github.com>
2023-01-15 11:57:47 -0800

    login added

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA529Fa_14UhBfJFhanp7dCqz804u_KjIA",
+  authDomain: "amzn-2-yt-d5fb2.firebaseapp.com",
+  projectId: "amzn-2-yt-d5fb2",
+  storageBucket: "amzn-2-yt-d5fb2.appspot.com",
+  messagingSenderId: "64139398490",
+  appId: "1:64139398490:web:3d23bf39a02c9a525af763",
+};
